f6ff40dca37eede36e09662154989fb15570ccf4
Kristian Lyngstol <kristian@bohemians.org>
2010-08-16 00:10:53 +0200

    version.sh: take signed tags into consideration

--- version.sh
+++ version.sh
@@ -9,7 +9,7 @@ echo ":Copyright: Varnish Software AS 2010, Redpill Linpro AS 2008-2009"
 echo -n ":Version: "
 # UGLY as hell. Awk fetches anything relevant until the first version- tag,
 # the rest just strips pointless info.
-TMP=`git log --format='%h %d' | awk '/version/ { if (versionfound != 1)  { versionfound=1 ; print $0; } }; /\(/ { if (versionfound != 1) print $0 } ' | sed 's/origin\/[a-Z]*//g' | sed s/master// | sed s/,//g | sed 's/ *)/)/'`
+TMP=`git log --format='%h %d' | awk '/version/ { if (versionfound != 1)  { versionfound=1 ; print $0; } }; /\(/ { if (versionfound != 1) print $0 } ' | sed 's/origin\/[a-Z]*//g' | sed 's/\(master\|tag:\)//g' | sed s/,//g | sed 's/ *)/)/'`
 if [ $(echo "$TMP" | wc -l) -ne 1 ]; then
 	echo -n "untagged draft version "
 	echo $TMP
