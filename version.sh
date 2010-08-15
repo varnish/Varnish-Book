2e9b7fec57321f3f95c7fbe7d6c03a51e7f1d146
Kristian Lyngstol <kristian@bohemians.org>
2010-08-15 22:48:39 +0200

    Fix version-script

--- version.sh
+++ version.sh
@@ -7,7 +7,9 @@
 echo ":Author: Tollef Fog Heen, Kristian Lyngstøl"
 echo ":Copyright: Varnish Software AS 2010, Redpill Linpro AS 2008-2009"
 echo -n ":Version: "
-TMP=`git log --format='%h %d' | grep HEAD | sed 's/origin\/[a-Z]*//g' | sed s/master// | sed s/,//g | sed 's/ *)/)/'| head -n2`
+# UGLY as hell. Awk fetches anything relevant until the first version- tag,
+# the rest just strips pointless info.
+TMP=`git log --format='%h %d' | awk '/version/ { if (versionfound != 1)  { versionfound=1 ; print $0; } }; /\(/ { if (versionfound != 1) print $0 } ' | sed 's/origin\/[a-Z]*//g' | sed s/master// | sed s/,//g | sed 's/ *)/)/'| head -n2`
 if [ $(echo "$TMP" | wc -l) -ne 1 ]; then
 	echo -n "untagged/unreleased draft version "
 	echo $TMP
