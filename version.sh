a62487967335c6e545b20d9bb7f610daa2f4a35a
Kristian Lyngstol <kristian@bohemians.org>
2011-01-17 22:35:55 +0100

    build: Simplify the version.sh script
    Should ensure better tag-names even on cloned repos and whatever not...
    First real test is after this commit.

--- version.sh
+++ version.sh
@@ -6,16 +6,13 @@
 
 echo ":Author: Tollef Fog Heen, Kristian Lyngstøl"
 echo ":Copyright: Varnish Software AS 2010, Redpill Linpro AS 2008-2009"
-echo -n ":Version: "
-# UGLY as hell. Awk fetches anything relevant until the first version- tag,
-# the rest just strips pointless info.
-TMP=`git log --format='%h %d' | awk '/version/ { if (versionfound != 1)  { versionfound=1 ; print $0; } }; /\(/ { if (versionfound != 1) print $0 } ' | sed 's/origin\/[a-Z]*//g' | sed 's/\(master\|tag:\)//g' | sed s/,//g | sed 's/ *)/)/'`
-if [ $(echo "$TMP" | wc -l) -ne 1 ]; then
-	echo -n "untagged draft version "
-	echo $TMP
-else
-	echo $TMP | sed 's/.*(//' | sed 's/).*//' | sed 's/HEAD//' | sed 's/ version-//'
+
+VER=`git tag -l --contains | sed s/version-//`
+if [ -z $VER ]; then
+	VER="untagged draft version $(git log --format '%h' | head -n1)"
 fi
+
+echo ":Version: $VER"
 echo -n ":Date: "
 git log --format="%ad" --date=short | head -n1
 
