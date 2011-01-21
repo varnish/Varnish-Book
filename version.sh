aa9120b9f35b620c925003135b67022377b33e1e
Kristian Lyngstol <kristian@bohemians.org>
2011-01-21 15:08:12 +0100

    version.sh: Perfection! Sort of. And TODO-tweaks

--- version.sh
+++ version.sh
@@ -1,20 +1,42 @@
-#!/bin/sh
+#!/bin/bash
+# Generates RST for setting author, copyright, version.
 # Author: Kristian Lyngstol <kristian@varnish-software.com>
 #
-# Generates RST for setting author, copyright, version.
+# Copyright 2010,2011, Varnish Software AS 
+# Copyright 2008, Aron Griffis <agriffis@n01se.net>
+# Copyright 2008, Oracle
+#
+# Released under the GNU GPLv2
+#
+#
+# Note: The copyright for this script is probably irrelevant due to its
+# size, but the btrfs-snippet I (Kristian) finally ended up borrowing is
+# GPL2, thus I figure this script is also GPL2 if it ever matters.
+#
+# Note 2: The copyright that is echoed relates to the course material and
+# not the script.
+#
+# Note 3: I now have more comments than code here. Yai....
 
 echo ":Author: Tollef Fog Heen, Kristian Lyngstøl"
 echo ":Copyright: Varnish Software AS 2010-2011, Redpill Linpro AS 2008-2009"
+v="0.0"
 
-VER=`git tag -l --contains | sed s/version-// | head -n1`
+# Borrowed from btrfs version.sh (GPL2)
+which git &> /dev/null
+if [ $? == 0 -a -d .git ]; then
+    if head=`git rev-parse --verify HEAD 2>/dev/null`; then
+        if tag=`git describe --tags 2>/dev/null`; then
+            v="$tag"
+        fi
 
-# Not a tagged commit. Should probably trigger big red letters.
-if [ -z $VER ]; then
-	VER2="$(git tag -l | tail -n1)"
-	VER="untagged draft version $(git log --format='%h' | head -n1), following ${VER2}"
+        # Are there uncommitted changes?
+        git update-index --refresh --unmerged > /dev/null
+        if git diff-index --name-only HEAD >/dev/null; then
+            v="$v"-dirty
+        fi
+    fi
 fi
 
-echo ":Version: $VER"
-echo -n ":Date: "
-git log --format="%ad" --date=short | head -n1
-
+echo ":Version: ${v}"
+echo ":Date: " $(date +%Y-%m-%d)
