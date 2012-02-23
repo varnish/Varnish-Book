f026770200676029dc24696c9712cda46d8961da
Kristian Lyngstol <kristian@bohemians.org>
2012-02-23 17:23:39 +0100

    Add a chapter-picking script

---
+++ util/pickchapter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+awk -v include="$*" -v inctop="1" '
+BEGIN {
+	chapter=""
+	split(include,includea,",");
+}
+/^==+$/ {
+	if (length(last) == length($0)) {
+		 chapter=last;
+	}
+}
+{
+	for (a in includea) {
+		if (includea[a] == chapter) {
+			print last
+			break
+		}
+	}
+	if (inctop==1 && chapter == "")
+		print last
+}
+{ last = $0 }' varnish_sysadmin.rst
