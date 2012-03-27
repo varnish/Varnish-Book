947f62aba61c1084584d8425f6640f5e2d9b78c8
Kristian Lyngstol <kristian@bohemians.org>
2012-03-27 09:10:57 +0200

    Update attribution and other details

--- util/version.sh
+++ util/version.sh
@@ -12,17 +12,24 @@ which git &> /dev/null || exit 1
 v="$(git describe --always --dirty)"
 
 cat <<__EOF__
-:Authors: Tollef Fog Heen, Kristian Lyngstøl, Jérôme Renard
+:Authors: Tollef Fog Heen (\`Varnish Software\`_),
+          Kristian Lyngstøl (\`Varnish Software\`_),
+	  Jérôme Renard (\`39Web\`_)
 :Copyright: Varnish Software AS 2010-2012, Redpill Linpro AS 2008-2009
 :Versions: Documentation ${v} / Tested for Varnish ${varnishversion}
 :Date: $(date +%Y-%m-%d)
-:License: The material is available under a CC-BY-SA-NC license. See
+:License: The material is available under a CC-BY-NC-SA license. See
 	  http://creativecommons.org/licenses/by-nc-sa/3.0/ for the full
 	  license. For questions regarding what we mean by non-commercial,
 	  please contact training@varnish-software.com.
 :Contact: For any questions regarding this training material, please
           contact training@varnish-software.com.
+:Web:     http://www.varnish-software.com/book/
+:Source:  http://github.com/varnish/Varnish-Book/
 
+.. _Varnish Software: http://www.varnish-software.com/
+
+.. _39Web: http://39web.fr/
 
 __EOF__
 
